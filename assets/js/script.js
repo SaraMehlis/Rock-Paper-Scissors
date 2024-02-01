899a1e1926ee7d1030c0a0e0c77076fbde2086a7
Sara mehlis <Sara.mehlis@gmail.com>
2024-02-01 16:12:06 +0000

    Add computer options

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const game = () => {
     let pscore = 0;
     let cscore = 0;
 
-
+    //start  the game
     const startGame = () => {
         const playBtn = document.querySelector('.intro button');
         const introscreen = document.quarySelector('.intro');
@@ -11,11 +11,31 @@ const game = () => {
         //everytime we click lets play something will appear
         playBtn.addEventListener('click', () => {
             introscreen.classList.add('fadeOut')
+            match.classList.add('fadeIn');
         });
     };
+    //play match
+    const playMatch = () => {
+        const options = document.querySelectorAll('options button');
+        const playerHand = document.quarySelector('.player-hand');
+        const computerHand = document.quarySelector('.computer-hand');
+        //computer options , generate random number between 0-1
+        const computerOptions = ['rock', 'paper, scissors'];
+        options.forEach(option => {
+            option.addEventListener('click', function(){
+                const computerNumber = Math.floor(Math.random() * 3)
+                const computerChoise = computerOptions[computerNumber];
+           
+        });
+    });
+};
+
+
+
     //call all the inner function
     startGame();
-}
+    playMatch();
+};
 
 //start game fun
 game();
