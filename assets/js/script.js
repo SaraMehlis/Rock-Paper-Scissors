2a9342a8f9c877724678de2879d8947204d62bbf
Sara mehlis <Sara.mehlis@gmail.com>
2024-02-01 19:40:41 +0000

    Add game options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,114 @@
 //self containing code to avoid global variables
 const game = () => {
-    let pscore = 0;
-    let cscore = 0;
+    let pScore = 0;
+    let cScore = 0;
 
     //start  the game
     const startGame = () => {
         const playBtn = document.querySelector('.intro button');
-        const introscreen = document.quarySelector('.intro');
-        const match = document.quarySelector('.match');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
         //everytime we click lets play something will appear
         playBtn.addEventListener('click', () => {
-            introscreen.classList.add('fadeOut')
+            introScreen.classList.add('fadeOut')
             match.classList.add('fadeIn');
         });
     };
     //play match
     const playMatch = () => {
-        const options = document.querySelectorAll('options button');
-        const playerHand = document.quarySelector('.player-hand');
-        const computerHand = document.quarySelector('.computer-hand');
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        const hands = document.querySelectorAll(".hands img");
+
+        hands.forEach(hand => {
+            hand.addEventListener("animationend", function() {
+              this.style.animation = "";
+            });
+          });
+      
         //computer options , generate random number between 0-1
         const computerOptions = ['rock', 'paper, scissors'];
         options.forEach(option => {
             option.addEventListener('click', function(){
                 const computerNumber = Math.floor(Math.random() * 3)
-                const computerChoise = computerOptions[computerNumber];
-           
+                const computerChoice = computerOptions[computerNumber];
+           //call compare hands
+
         });
     });
 };
 
 
+setTimeout(() => {
+    //Here is where we call compare hands
+    compareHands(this.textContent, computerChoice);
+
+    //update images ????
+    playerHand.src = `./assets/${this.textContent}.png`;
+    computerHand.src = `./assets/${computerChoice}.png`;
+}, 2000);
+
+
+//animation
+playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
+    });
+};
+
+const compareHands = (playerChoice, computerChoice) =>{
+    //update text
+    const winner = document.querySelector('.winner');
+    //if tie
+    if(playerChoice === computerChoice){
+        winner.textContent = 'It is a tie';
+        return;
+    }
+//if rock
+if(playerChoice === 'rock'){
+    if(computerChoise === ' scissors'){
+        winner.textContent = ' player wins';
+        pScore++;
+        updateScore();
+        return;
+    }else{
+        winner.textContent = 'computer wins';
+        return;
+    }
+}
+//if paper
+if (playerChoice === "paper") {
+    if (computerChoice === "scissors") {
+      winner.textContent = "Computer Wins";
+      cScore++;
+      updateScore();
+      return;
+    } else {
+      winner.textContent = "Player Wins";
+      pScore++;
+      updateScore();
+      return;
+    }
+}
+
+
+//if scissors
+if(playerChoice === 'scissors'){
+    if(computerChoise === ' rock'){
+        winner.textContent = 'computer Wins';
+        cScore++;
+        updateScore(); 
+        return;
+    }else{
+        winner.textContent = 'player Wins';
+        pScore++;
+        updateScore();
+        return;
+    }
+}
+};
 
     //call all the inner function
     startGame();
